f9925c9bc17cc8ba66979a4006fa0095e422a319
pranav6487 <pranav6487@gmail.com>
2015-12-20 19:38:40 +0530

    users class

--- includes/pages/login.php
+++ includes/pages/login.php
@@ -5,7 +5,7 @@ global $page_params;
 $pageArgs = $global_params['page_arguments'];
 
 if( $pageArgs['aj'] == 1 && $pageArgs['action'] == "login" ) {
-    
+    $users = new Users();
     echo json_encode(array("status" => 1));
     exit;
 }
