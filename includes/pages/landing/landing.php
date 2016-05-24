5cfc4244fbd4eb70578221cde09a02707e72ccdc
pranav6487 <pranav6487@gmail.com>
2016-05-24 10:57:47 +0530

    return true or false on mail send

--- includes/pages/landing/landing.php
+++ includes/pages/landing/landing.php
@@ -8,17 +8,17 @@ if( $pageArgs['aj'] == 1 && $pageArgs['action'] == "contactMe" ) {
     $subject = "ManageMyResto - Inquiry from ".$pageArgs['name'];
     $message = $pageArgs['message'];
     $emailMsg = "Query from <br />Name: {$pageArgs['name']}<br />Email: {$pageArgs['email']}<br />Number: {$pageArgs['phone']}<br />Message:<br />".$message;
-    $header = "From:{$pageArgs['email']} \r\n";
+    $header = "";
+    $header .= "From:{$pageArgs['email']} \r\n";
     $header .= "Reply-to: ".$pageArgs['email']." \r\n";
     $header .= "MIME-Version: 1.0\r\n";
     $header .= "Content-type: text/html\r\n";
     $sendMail = mail(CONTACT_US_EMAIL,$subject,$emailMsg,$header);
     if( $sendMail == true ) {
-        echo "success";
+        return true;
     }
     else {
-        http_response_code(400);
-        echo "fail";
+        return false;
     }
     exit;
 }
